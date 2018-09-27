ed4eff64d0c06cf1ff69b0ef0b2a8f407580d2cb
irving199627 <irving_rosado01@hotmail.com>
2018-09-27 00:33:07 -0500

    usuario y token listo

---
+++ middlewares/autenticacion.js
@@ -0,0 +1,28 @@
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+/*  ======================================
+        Verificar token
+    ====================================== */
+exports.veficaToken = function(req, res, next) {
+    var token = req.query.token;
+
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Token Incorrecto',
+                errors: err
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+        // return res.status(200).json({
+        //     ok: true,
+        //     decoded: decoded
+        // });
+
+    });
+}
